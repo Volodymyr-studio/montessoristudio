5b0e908a4e99dccfc74c77a8bb00fdbc4e3f4c2f
Volodymyr-studio <hvg17.02@gmail.com>
2025-11-28 20:42:43 +0200

    Update backend.service.ts

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -7,23 +7,5 @@ import { environment } from './../environments/environment';
   providedIn: 'root',
 })
 export class BackendService {
-  private apiUrl = environment.backendUrl;
 
-  sendOrderApplianceDromForm(
-    token: string,
-    orderFormComponent: any
-  ): Observable<any> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      token: token,
-    });
-
-    return this.http.post<any>(
-      this.apiUrl,
-      JSON.stringify(orderFormComponent),
-      { headers }
-    );
-  }
-
-  constructor(private http: HttpClient) {}
 }
