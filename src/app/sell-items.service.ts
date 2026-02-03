03a1db2bb9395f19056eb35f47e315d727e2201d
Денис Хльобас <hvg17.02@gmail.com>
2026-02-03 22:18:24 +0200

    90

--- src/app/sell-items.service.ts
+++ src/app/sell-items.service.ts
@@ -7,76 +7,51 @@ export class SellItemsService {
   getItems() {
     return [
       {
-        cardTitle: 'Півтораспальний комплект',
+        cardTitle: 'розвиток дрібної та великої моторики',
         imgUrls: [
-          'photo/pv/1.jpeg',
-          'photo/pv/2.jpeg',
-          'photo/pv/3.jpeg',
-          'photo/pv/4.jpeg',
-          'photo/pv/5.jpeg',
-          'photo/pv/6.jpeg',
+          'photo/pv/banner.jpg'
         ],
-        price: '2 795 грн',
         description: [
-          'простирадло 200×240см',
-          'підковдра 160×220см',
-          'наволочка 50×70см — 2 шт',
+          'перші кроки до самостійності',
+          'розвиток комунікативних навичок та мовлення',
+          'середовище спеціально підготовлене',
         ],
       },
       {
-        cardTitle: 'Двоспальний комплект',
+        cardTitle: 'розвиток дрібної та великої моторики',
         imgUrls: [
-          'photo/dv/1.jpeg',
-          'photo/dv/2.jpeg',
-          'photo/dv/3.jpeg',
-          'photo/dv/4.jpeg',
-          'photo/dv/5.jpeg',
-          'photo/dv/6.jpeg',
-          'photo/dv/7.jpeg',
+          'photo/pv/banner.jpg'
         ],
-        price: '3 120 грн',
         description: [
-          'простирадло 220×260см',
-          'підковдра 180×220см',
-          'наволочка 50×70см — 2шт',
+          'перші кроки до самостійності',
+          'розвиток комунікативних навичок та мовлення',
+          'середовище спеціально підготовлене',
         ],
       },
       {
-        cardTitle: 'Євро',
+        cardTitle: 'розвиток дрібної та великої моторики',
         imgUrls: [
-          'photo/er/2.jpeg',
-          'photo/er/3.jpeg',
-          'photo/er/4.jpeg',
-          'photo/er/5.jpeg',
-          'photo/er/6.jpeg',
-          'photo/er/7.jpeg',
+          'photo/pv/banner.jpg'
         ],
-        price: '3 445 грн',
         description: [
-          'простирадло 240×260см',
-          'підковдра 200×220см',
-          'наволочка 50×70см — 2шт',
+          'перші кроки до самостійності',
+          'розвиток комунікативних навичок та мовлення',
+          'середовище спеціально підготовлене',
         ],
       },
       {
-        cardTitle: 'Сімейний',
+        cardTitle: 'розвиток дрібної та великої моторики',
         imgUrls: [
-          'photo/sm/1.jpeg',
-          'photo/sm/2.jpeg',
-          'photo/sm/3.jpeg',
-          'photo/sm/4.jpeg',
-          'photo/sm/5.jpeg',
-          'photo/sm/6.jpeg',
+          'photo/pv/banner.jpg'
         ],
-        price: '4 310 грн',
         description: [
-          'простирадло 240×260см',
-          'підковдра 160×220см — 2шт',
-          'наволочка 50×70см — 2шт',
+          'перші кроки до самостійності',
+          'розвиток комунікативних навичок та мовлення',
+          'середовище спеціально підготовлене',
         ],
       },
     ];
   }
 
-  constructor() {}
+  constructor() { }
 }
